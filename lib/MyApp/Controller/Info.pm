812f2c73a019cef813efde4a0dc20aa755ee05e3
slegga <steihamm@gmail.com>
2016-10-12 16:44:16 +0000

    Working Info. Next Index

--- lib/MyApp/Controller/Info.pm
+++ lib/MyApp/Controller/Info.pm
@@ -25,18 +25,18 @@ sub info {
   my $text = $self->tx->req->headers->to_string; #to_hash
   my $html = $conv->process_chunk($text);
 
-  return $self->render(html=>$html);
+  return $self->render(text=>$html,format=>'html');
 }
 
 sub show_pi_status {
   my $self = shift;
   my $file = $self->config->{'pi-status-file'};
-  return $self->render(text => $file);
+#  return $self->render(text => $file);
   my $text = read_file($file);  
   my $html = $conv->process_chunk($text);
 #  warn $text;
 #  $text = cr2br($text);
-  return $self->render(html => $html );
+  return $self->render(text => $html, format =>'html' );
 }
 
 
