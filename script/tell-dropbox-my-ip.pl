f83f5b7c445ccb44ef8a668d0c2ccb1911d6ad1f
slegga <steihamm@gmail.com>
2016-12-15 19:10:18 +0000

    Fikser temp

--- script/tell-dropbox-my-ip.pl
+++ script/tell-dropbox-my-ip.pl
@@ -32,12 +32,12 @@ my $uname = `uname -a`;
 my $cels;
 if ($uname=~/raspb/i) {
      $cels=`/opt/vc/bin/vcgencmd measure_temp`;
-  ($value_hr->{temp}) = ($cels=~/temp\=([\d\.\,\w])+/);
+  ($value_hr->{temp}) = ($cels=~/temp\=([\d\.\,\w\-\+]+.\w)/);
 } elsif ($uname=~/msys/i) {
     $cels='';
 } else {
     $cels =`sensors`;
-  ($value_hr->{temp}) = ($cels=~/temp1\:\s+([\d\.\,\w\-\+]+.\w)/);
+  ($value_hr->{temp}) = ($cels=~/temp1\:\s+([\d\.\,\w\-\+]+)/);
 }
 $value_hr->{time} = strftime("%Y-%m-%d %H:%M:%S", localtime);
 printf j( $value_hr);
