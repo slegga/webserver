80fd1ae962388668b08533a28e0c8048b7dce0a5
Slegga <sslegga@gmail.com>
2017-05-22 06:40:00 +0200

    toadfarm fikling

--- script/toadfarm.pl
+++ script/toadfarm.pl
@@ -12,7 +12,18 @@ logging {
         file     => "/var/log/toadfarm/pi-webserver.log",
         level    => "info",
 };
+# TODO Must fix conf file first. Try on liveserver
+# mount 'API::PiData' => {mount_point => '/api'};
 mount "MyApp";
 
 plugin "Toadfarm::Plugin::AccessLog";
+
+my @chars = ("A".."Z", "a".."z");
+
+# May unvalidate all connections
+my $string;
+$string .= $chars[rand @chars] for 1..64;
+secrets ( $string ); 
+
 start ['http://*:8888'], workers => 1;
+
