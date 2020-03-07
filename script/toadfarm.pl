71f8978eaf8655d58353ba38f4ff6f43415ec98b
Stein Hammer <steihamm@gmail.com>
2020-03-07 17:42:24 +0100

    Prøver å fikse toadfarm.pl

--- script/toadfarm.pl
+++ script/toadfarm.pl
@@ -1,7 +1,21 @@
 #!/usr/bin/env perl
 
 use Toadfarm -init;
-use lib "../utilities-perl/lib";
+use Mojo::File 'path';
+use Mojo::Util 'dumper';
+my $lib;
+BEGIN {
+    my $gitdir = Mojo::File->curfile;
+    my @cats = @$gitdir;
+    while (my $cd = pop @cats) {
+        if ($cd eq 'git') {
+            $gitdir = path(@cats,'git');
+            last;
+        }
+    }
+    $lib =  $gitdir->child('utilities-perl','lib')->to_string; #return utilities-perl/lib
+};
+use lib $lib;
 use SH::UseLib;
 
 =head1 NAME
@@ -21,7 +35,7 @@ logging {
         level    => "info",
 };
 # TODO Must fix conf file first. Try on liveserver
-mount 'API::PiData' => {mount_point => '/api/home/'};
+mount 'SH::PiOpenAPI' => {mount_point => '/api/home/'};
 mount 'API::Piano' => {mount_point => '/api/piano/'};
 my $priv = '/privat/';
 mount "MyApp" => {mount_point => $priv};
