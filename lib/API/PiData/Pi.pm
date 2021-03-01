5e8385520cb99cff7f3dbfdff5a5839d0d49f820
Stein Hammer <steihamm@gmail.com>
2021-03-01 18:18:50 +0100

    Går over til csv. Mer skrive vennlig.

--- lib/API/PiData/Pi.pm
+++ lib/API/PiData/Pi.pm
@@ -1,6 +1,6 @@
 package API::PiData::Pi;
 use Mojo::Base "Mojolicious::Controller";
-use YAML::Tiny 'Dump';
+# use YAML::Tiny 'Dump';
 use autodie;
 use Mojo::JSON;
 use YAML::Syck;
@@ -30,6 +30,8 @@ This is a controller class. Ment to be dispatched to from mojolicious applicatio
 =head2 set_pi_data
 
 Store pi data to file.
+
+And store history in a csv file.
 
 =cut
 
@@ -61,24 +63,22 @@ sub set_pi_data {
 	my $body = $c->validation->output->{body};
 	say STDERR Dump $c->config;
 	my $pi_file = $c->config->{'datadir'} ? $c->config->{'datadir'}.'/pi-status.json' : $ENV{HOME}.'/etc/pi-status.json';
+    {
+    	open my $fh,'>', $pi_file;
+    	print $fh Mojo::JSON::to_json($body);
+    	close $fh;
+    }
+	my $pi_history = $c->config->{'datadir'} ? $c->config->{'datadir'}.'/pi-history.csv' : $ENV{HOME}.'/etc/pi-history.csv';
 
-	open my $fh,'>', $pi_file;
-	print $fh Mojo::JSON::to_json($body);
-	close $fh;
-
-	my $pi_history = $c->config->{'datadir'} ? $c->config->{'datadir'}.'/pi-history.yml' : $ENV{HOME}.'/etc/pi-history.yml';
-
-	# readfile, add data, sort, write
-	my $history_yaml=();
-	$history_yaml = LoadFile("$pi_history") if -f "$pi_history";
-	my $temp = $body->{temp};
-	$temp =~ s/\D+$//g;
-	push (@$history_yaml,[Mojo::Date->new($body->{a_time})->epoch, $temp]);
-
-	#@$history_yaml = sort{$a->[0] <=> $b[0]} @$history_yaml;
-	DumpFile("$pi_history",$history_yaml);
-  # $output will be validated by the OpenAPI spec before rendered
-  $c->render(openapi => 'ok');
+	# write
+	{
+    	my $temp = $body->{temp};
+    	$temp =~ s/\D+$//g;
+    	open my $fh,'>>',$pi_history;
+    	printf $fh "%s;%s",Mojo::Date->new($body->{a_time})->epoch, $temp;
+        close $fh;
+    }
+    $c->render(openapi => 'ok');
 }
 
 1;
