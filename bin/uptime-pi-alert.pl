5e5149d379e8fd0ee4e104c21ec52866192b222e
Stein Hammer <steihamm@gmail.com>
2021-04-15 16:51:51 +0200

    øker fra 11 til 15 timer

--- bin/uptime-pi-alert.pl
+++ bin/uptime-pi-alert.pl
@@ -60,11 +60,11 @@ sub main {
 #    say STDERR $time;
     my $last_pi_epoch=Mojo::Date->new($time )->epoch;
 
-    my $old_epoch = time - 11 * 60 * 60; # 6 hour downtime at night
+    my $old_epoch = time - 15  * 60 * 60; # 6 hour downtime at night
     if ($last_pi_epoch < $old_epoch) {
         say "Pi down. Last call at $time";
     }
-    say $last_pi_epoch,'  ', $old_epoch;
+    say $last_pi_epoch,' <  ', $old_epoch;
 }
 
 __PACKAGE__->new(options_cfg=>{extra=>1})->main();
